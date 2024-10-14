795aa684f628f53f0d95bafccac6c853ab08f12e
Lumeris4 <l20langlois@gmail.com>
2024-10-14 20:37:57 +0200

    Update ft_memset.c

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -10,22 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
 void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t			i;
-	char			*str;
+	size_t					i;
+	unsigned char			*str;
 
 	i = 0;
-	str = (char *)s;
+	str = (unsigned char *)s;
 	while (i != n)
 	{
-		str[i] = (char)c;
+		str[i] = (unsigned char)c;
 		i++;
 	}
 	return (s);
 }
+/*
+#include <stdio.h>
+int main()
+{
+	char str[6] = "essai";
+	size_t n = 5;
+	ft_memset((void *)str, '4',n);
+	printf("%s",str);
+}
+*/
 /*
 #include <stdio.h>
 int main()
